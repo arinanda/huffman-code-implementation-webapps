8577e5091e7c7850dcf12ba90750211c4f450c9f
Ronald Sumbayak <ronaldsumbayak611@gmail.com>
2018-10-12 20:27:28 +0700

    Cleanup

--- storage/compressor/app.py
+++ storage/compressor/app.py
@@ -1,10 +1,16 @@
 import sys
 
+# noinspection PyUnresolvedReferences
 from huffman.huffman.compress import compress as huffman_compress
+# noinspection PyUnresolvedReferences
 from huffman.huffman.decompress import decompress as huffman_decompress
+# noinspection PyUnresolvedReferences
 from huffman.adaptive_huffman.compress import compress as adaptive_huffman_compress
+# noinspection PyUnresolvedReferences
 from huffman.adaptive_huffman.decompress import decompress as adaptive_huffman_decompress
+# noinspection PyUnresolvedReferences
 from huffman.tunstall.compress import compress as tunstall_compress
+# noinspection PyUnresolvedReferences
 from huffman.tunstall.decompress import decompress as tunstall_decompress
 
 if __name__ == '__main__':
